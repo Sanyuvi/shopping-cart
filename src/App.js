c0df8002bd3394dbde58c6e6c2b38bdba23ccd3a
Sanyuvi <santhoshh2197@gmail.com>
2023-08-11 17:09:34 +0530

    shopping cart completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import CardGrid from "./Componenets/card";
+import Header from "./Componenets/header";
+import Navbar from "./Componenets/navbar";
+import Footer from "./Componenets/footer";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function App() {
+  const [cartValue, setCartValue] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar cartValue={cartValue} />
+      <Header />
+      <section>
+        <div className="container">
+          <div className="row">
+            <CardGrid setCartValue={setCartValue}></CardGrid>
+          </div>
+        </div>
+        <Footer />
+      </section>
+    </>
   );
 }
 
